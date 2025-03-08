d1e501a77c862aad1936e782b7b98a6725c4b04a
Adigun Bukunmi <bukunmiadigun14@gmail.com>
2025-03-08 05:35:42 +0100

    update: thank-you

--- src/stepsComponents/Thanks.jsx
+++ src/stepsComponents/Thanks.jsx
@@ -1,8 +1,10 @@
+import thankYou from "../assets/icon-thank-you.svg"
 export default function Thanks(){
+
     return(
         <section className="thank-you-section">
             <div >
-            <img src="/public/images/icon-thank-you.svg" alt="" />
+            <img src={thankYou} alt="" />
             <h1>Thank you!</h1>
             <p>Thanks for confirming your subscription!
                 We hope you have fun using our platform. If you need support,
